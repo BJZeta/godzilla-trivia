2ca22aa8809065f0e164066acc995d7c93696491
Brandon U <unzueta.brandon@gmai.com>
2020-10-13 19:59:47 -0700

    creating fuctions runGame, questionMaker and checkAnswer

--- scripts.js
+++ scripts.js
@@ -1,13 +1,61 @@
+const menu = document.getElementById('menu');
+const start = document.getElementById('start');
+const gameScreen = document.getElementById('game-screen');
+const questionSec = document.getElementById('question');
+const answerSec = document.getElementById('answer-section');
+const answerBtn = document.getElementById('answerbtn');
 $.ajax({
     type: "Get",
     url: "questions.json",
     dataType: "json",
     success: function(data) {
-        return runGame(data);
+
+        const questions = data;
+
+        ///////////////////////Starts Game
+        function runGame() {
+            menu.innerHTML = "";
+            let questionCount = 1;
+            if(questions[0]) {
+                questionMaker(questionCount);
+            } else {
+                finalScreen();
+            }
+        }
+
+        ////////////////////////////////////////Sets up question board
+        function questionMaker(questionCount) { 
+            const question = questions[0];  
+            const answers = question.answers;
+            const correctAnswer = question.correctAnswer;
+        
+            console.log(questions);
+        
+            questionSec.innerHTML = `
+                <h3>Question ${questionCount}<h3>
+                    <p>${question.question}<p>
+            `
+        
+            const allAnswers = answers.map(answer => {
+                return `<button onclick="checkAnswer(${answer}, ${correctAnswer})">${answer}</button>`
+            });
+        
+            answerSec.innerHTML = allAnswers;
+        
+        }
+
+        /////////////////////////////////////////////Start Button
+        start.addEventListener('click', () => runGame());
     },
     error: () => alert("didnt work")
 })
 
-function runGame(data) {
-    console.log(data);
+/////////////////////////////////////////////Check if Answer is Correct
+function checkAnswer(selectedAns, correctAns) {
+    if(selectedAns === correctAns) {
+        console.log("correct");
+    } else {
+        console.log("incorrect");
+    }
 }
+
